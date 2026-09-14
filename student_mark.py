009a54d29b8e8e9a2d5011c6d402d3b5f79276bc
Aung Sann Oo <theflash1321@gmail.com>
2026-09-14 13:58:18 +0630

    Fix average calculation in student_mark.py

--- student_mark.py
+++ student_mark.py
@@ -5,7 +5,7 @@ mark1 = float(input("Enter mark for subject 1: "))
 mark2 = float(input("Enter mark for subject 2: "))
 
 
-average = (mark1 + mark2) / 5 +3
+average = (mark1 + mark2) / 5 +4
 
 print("\nStudent Name:", name)
 print("Average Mark:", average)
