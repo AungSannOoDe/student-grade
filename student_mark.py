d08467165f8c5ff3a4f07b13b533fc75e079939b
AungSannOoDe <theflash1321@gmail.com>
2026-09-14 13:41:04 +0630

    added grade.py

---
+++ student_mark.py
@@ -0,0 +1,11 @@
+
+name = input("Enter student's name: ")
+
+mark1 = float(input("Enter mark for subject 1: "))
+mark2 = float(input("Enter mark for subject 2: "))
+
+
+average = (mark1 + mark2) / 2
+
+print("\nStudent Name:", name)
+print("Average Mark:", average)
